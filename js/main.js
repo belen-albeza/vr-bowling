ace952811c75964485e1e9dcd723e7104590a1ae
Belén Albeza <belen.albeza@gmail.com>
2017-04-21 16:31:35 +0200

    no lock wip

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 var shallLaunch = false;
+var hasLaunched = false;
 var ball = document.getElementById('ball');
-var hand = document.getElementById('hand');
+var hand = document.getElementById('right');
 var handLock = document.getElementById('hand-lock');
 
 var lastPosition = new CANNON.Vec3(0, 0, 0);
 var currentPosition = new CANNON.Vec3(0, 0, 0);
 
 function launch() {
-    ball.removeAttribute('constraint');
-    console.log('LAUnch!');
+    hasLaunched = true;
+    // ball.removeAttribute('constraint');
+    // currentPosition.copy(hand.getAttribute('position'));
+    let delta = 1/60.0;
+    let velocity = currentPosition.vsub(lastPosition).scale(10);
+    ball.body.applyLocalImpulse(velocity, new CANNON.Vec3(0, 0, 0));
+    console.log('buuuuu');
 }
 
 AFRAME.registerComponent('throwing-hand', {
+    dependencies: ['dynamic-body'],
     tick: function () {
-        handLock.setAttribute('position', this.el.getAttribute('position'));
+        // handLock.setAttribute('position', this.el.getAttribute('position'));
+        let position = this.el.getAttribute('position');
+        if (!hasLaunched && ball.body) {
+            ball.body.velocity.set(0, 0, 0);
+            ball.body.angularVelocity.set(0, 0, 0);
+            ball.body.position.set(position.x, position.y, position.z);
+        }
 
         if (shallLaunch) {
             shallLaunch = false;
@@ -26,7 +39,9 @@ window.onload = function () {
     document.addEventListener('keydown', function (e) {
         if (e.keyCode === 32) {
             e.preventDefault();
-            shallLaunch = true;
+            if (!hasLaunched) {
+                shallLaunch = true;
+            }
         }
     });
 };
