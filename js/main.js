a4bdeacd1490bc901487d750ad35597995435339
Belén Albeza <belen.albeza@gmail.com>
2017-04-21 18:31:52 +0200

    fix speed and positions

--- js/main.js
+++ js/main.js
@@ -9,12 +9,10 @@ var currentPosition = new CANNON.Vec3(0, 0, 0);
 
 function launch() {
     hasLaunched = true;
-    // ball.removeAttribute('constraint');
-    // currentPosition.copy(hand.getAttribute('position'));
     let delta = 1/60.0;
-    let velocity = currentPosition.vsub(lastPosition).scale(10);
+    let velocity = currentPosition.vsub(lastPosition).scale(1/delta).scale(10);
+    console.log(velocity);
     ball.body.applyLocalImpulse(velocity, new CANNON.Vec3(0, 0, 0));
-    console.log('buuuuu');
 }
 
 AFRAME.registerComponent('throwing-hand', {
@@ -22,16 +20,20 @@ AFRAME.registerComponent('throwing-hand', {
     tick: function () {
         // handLock.setAttribute('position', this.el.getAttribute('position'));
         let position = this.el.getAttribute('position');
+
         if (!hasLaunched && ball.body) {
             ball.body.velocity.set(0, 0, 0);
             ball.body.angularVelocity.set(0, 0, 0);
             ball.body.position.set(position.x, position.y, position.z);
         }
 
-        if (shallLaunch) {
+        if (shallLaunch && ball.body) {
             shallLaunch = false;
+            currentPosition.copy(position);
             launch();
         }
+
+        lastPosition.copy(position);
     }
 });
 
