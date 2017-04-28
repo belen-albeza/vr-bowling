78770366e88cef2f7cb2b0b45d5be8cfde0ed44a
Belén Albeza <belen.albeza@gmail.com>
2017-04-28 17:29:55 +0200

    cleanup and contact material

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
-var shallLaunch = false;
-var hasLaunched = false;
+var shallThrow = false;
+var hasThrown = false;
 var ball;
-var hand;
 var lastPosition = new CANNON.Vec3(0, 0, 0);
 var currentPosition = new CANNON.Vec3(0, 0, 0);
 var hasPrepared = false;
 
-function launch() {
-    hasLaunched = true;
+function throwBall() {
+    hasThrown = true;
     hasPrepared = false;
     let delta = 1/60.0;
-    let velocity = currentPosition.vsub(lastPosition).scale(1/delta).scale(50);
-    console.log(velocity);
-    ball.body.applyLocalImpulse(velocity, new CANNON.Vec3(0, 0, 0));
+    let velocity = currentPosition.vsub(lastPosition).scale(1/delta);
+    ball.body.applyLocalImpulse(velocity.scale(50), new CANNON.Vec3(0, 0, 0));
 }
-
-AFRAME.registerSystem('bowling', {
-});
 
 AFRAME.registerComponent('throwing-hand', {
     dependencies: ['dynamic-body', 'vive-controls'],
@@ -26,32 +21,33 @@ AFRAME.registerComponent('throwing-hand', {
             console.log('triggerdown');
         });
         this.el.addEventListener('triggerup', function (e) {
-            if (!hasLaunched && hasPrepared) { shallLaunch = true};
+            if (!hasThrown && hasPrepared) { shallThrow = true};
             console.log('triggerup');
         });
         this.el.addEventListener('menuup', function (e) { // restart
-            hasLaunched = false;
+            hasThrown = false;
             hasPrepared = false;
-            shallLaunch = false;
+            shallThrow = false;
             lastPosition = new CANNON.Vec3(0, 0, 0);
             currentPosition = new CANNON.Vec3(0, 0, 0);
         });
+
     },
     tick: function () {
         // handLock.setAttribute('position', this.el.getAttribute('position'));
         let position = this.el.getAttribute('position');
 
-        if (!hasLaunched && ball && ball.body) {
+        if (!hasThrown && ball && ball.body) {
             ball.body.velocity.set(0, 0, 0);
             ball.body.angularVelocity.set(0, 0, 0);
             ball.body.quaternion.set(0, 0, 0, 1);
             ball.body.position.set(position.x, position.y, position.z);
         }
 
-        if (shallLaunch && ball && ball.body) {
-            shallLaunch = false;
+        if (shallThrow && ball && ball.body) {
+            shallThrow = false;
             currentPosition.copy(position);
-            launch();
+            throwBall();
         }
 
         lastPosition.copy(position);
@@ -60,5 +56,20 @@ AFRAME.registerComponent('throwing-hand', {
 
 window.onload = function () {
     ball = document.getElementById('ball');
-    hand = document.getElementById('right');
+
+    //
+    // make the ball not bounce when colliding with the ground or the bumpers
+    //
+
+    let world = document.querySelector('a-scene').systems['physics'].world;
+    let groundMaterial = document.querySelector('[static-body]').body.material;
+    let laneObjects = document.querySelectorAll(['static-body']);
+    for (let i = 0; i < laneObjects.length; i++) {
+        laneObjects[i].body.material = groundMaterial;
+    }
+
+    // create a contact material: ball vs ground/bumpers
+    let contactMaterial = new CANNON.ContactMaterial(
+        groundMaterial, ball.body.material, { restitution: 0.001 });
+    world.addContactMaterial(contactMaterial);
 };
